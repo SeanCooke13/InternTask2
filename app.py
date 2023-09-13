991df5bc9b4a210efa4f9b14de2899b2cb3f7151
Kyle Calip <34113827+KCalip@users.noreply.github.com>
2023-09-13 13:58:39 -0700

    This is a big step in developing the application. I have created a structured UI using QT Designer as well as connected the text box to another label using a button. I will be able to manipulate the user input and generate the intended output from here.

--- app.py
+++ app.py
@@ -1,16 +1,78 @@
-from PySide2.QtWidgets import QApplication, QWidget
+# -*- coding: utf-8 -*-
 
-# Only needed for access to command line arguments
-import sys
+################################################################################
+## Form generated from reading UI file 'Task2.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
 
-# You need one (and only one) QApplication instance per application.
-# Pass in sys.argv to allow command line arguments for your app.
-# If you know you won't use command line arguments QApplication([]) works too.
-app = QApplication(sys.argv)
+from PySide2.QtCore import (QCoreApplication, QDate, QDateTime, QMetaObject,
+    QObject, QPoint, QRect, QSize, QTime, QUrl, Qt)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+    QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter,
+    QPixmap, QRadialGradient)
+from PySide2.QtWidgets import *
 
-# Create a Qt widget, which will be our window.
-window = QWidget()
-window.show()  # IMPORTANT!!!!! Windows are hidden by default.
 
-# Start the event loop.
-app.exec_()
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(843, 722)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.textBrowser = QTextBrowser(self.centralwidget) #This is just the prompt
+        self.textBrowser.setObjectName(u"textBrowser")
+        self.textBrowser.setGeometry(QRect(0, 0, 661, 41))
+        self.lineEdit = QLineEdit(self.centralwidget) #This is the text box where the number will be inputted
+        self.lineEdit.setObjectName(u"lineEdit")
+        self.lineEdit.setGeometry(QRect(0, 50, 113, 20))
+        self.Input = QPushButton(self.centralwidget)
+        self.Input.setObjectName(u"Input") #This is the button for submitting the number
+        self.Input.setGeometry(QRect(120, 50, 75, 23))
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label") #telling the user where the output will be
+        self.label.setGeometry(QRect(0, 80, 111, 16))
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(110, 80, 51, 16)) #the output i think
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 843, 21))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.Input.clicked.connect(self.receive_input)
+
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.textBrowser.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">Please insert any number you would like to see the prime factorizaton of!</span></p></body></html>", None))
+        self.Input.setText(QCoreApplication.translate("MainWindow", u"Go!", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"Prime Factorization ->", None))
+        self.label_2.setText("")
+    # retranslateUi
+    def receive_input(self):
+        self.label_2.setText(self.lineEdit.text())
+if __name__ == "__main__":
+    import sys
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
