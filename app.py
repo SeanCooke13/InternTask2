d321e38998b50008483b7d9da68549d83c24c1ee
Kyle Calip <34113827+KCalip@users.noreply.github.com>
2023-09-18 13:17:44 -0700

    I was able to create a Gen file that can be altered to make version testing much simpler. I also added all of the logic to the application on the app.py file. Overall, we are in the finishing up stage for this project.

--- app.py
+++ app.py
@@ -8,6 +8,7 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 import sys
+import math
 
 from PySide2.QtCore import (QCoreApplication, QDate, QDateTime, QMetaObject,
     QObject, QPoint, QRect, QSize, QTime, QUrl, Qt)
@@ -32,17 +33,34 @@ class Prime_Factorizer(object):
 "</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
 "<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
 "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">Please insert any number you would like to see the prime factorizaton of!</span></p></body></html>", None))
-        self.unser_input.setText("")
+        self.user_input.setText("")
         self.go_button.setText(QCoreApplication.translate("MainWindow", u"Go!", None))
         self.output_label.setText(QCoreApplication.translate("MainWindow", u"  Prime Factorization :", None))
         self.prime_factors.setText("")
     def receive_input(self):
-        self.prime_factors.setText(self.unser_input.text())
-    
+        value = int(self.user_input.text())
+        cap = math.floor(math.sqrt(value))
+        primes = [2]
+        output = ""
+        for x in range(3,cap+1):
+            is_a_prime = True
+            for y in primes:
+                if(x%y==0):
+                    is_a_prime = False
+            if is_a_prime:
+                primes.append(x)
+        while len(primes):
+            if (value%primes[-1]==0):
+                output = output + str(primes[-1]) + " "
+                value = value/primes[-1]
+            else:
+               primes.remove(primes[-1]) 
+        if (value!=1):
+            output = output + str(int(value))
+        self.prime_factors.setText(output)
 
 
 if __name__ == "__main__":
-    import sys
     app = QApplication(sys.argv)
     MainWindow = QMainWindow()
     ui = Prime_Factorizer()
--- gen/ui_app.py
+++ gen/ui_app.py
@@ -26,20 +26,20 @@ class Ui_MainWindow(object):
         self.textBrowser = QTextBrowser(self.centralwidget)
         self.textBrowser.setObjectName(u"textBrowser")
         self.textBrowser.setGeometry(QRect(0, 0, 761, 511))
-        self.widget = QWidget(self.centralwidget)
-        self.widget.setObjectName(u"widget")
-        self.widget.setGeometry(QRect(240, 70, 218, 48))
-        self.verticalLayout = QVBoxLayout(self.widget)
+        self.layoutWidget = QWidget(self.centralwidget)
+        self.layoutWidget.setObjectName(u"layoutWidget")
+        self.layoutWidget.setGeometry(QRect(240, 70, 218, 48))
+        self.verticalLayout = QVBoxLayout(self.layoutWidget)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.unser_input = QLineEdit(self.widget)
-        self.unser_input.setObjectName(u"unser_input")
+        self.user_input = QLineEdit(self.layoutWidget)
+        self.user_input.setObjectName(u"user_input")
 
-        self.horizontalLayout.addWidget(self.unser_input)
+        self.horizontalLayout.addWidget(self.user_input)
 
-        self.go_button = QPushButton(self.widget)
+        self.go_button = QPushButton(self.layoutWidget)
         self.go_button.setObjectName(u"go_button")
 
         self.horizontalLayout.addWidget(self.go_button)
@@ -49,12 +49,12 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_2 = QHBoxLayout()
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.output_label = QLabel(self.widget)
+        self.output_label = QLabel(self.layoutWidget)
         self.output_label.setObjectName(u"output_label")
 
         self.horizontalLayout_2.addWidget(self.output_label)
 
-        self.prime_factors = QLabel(self.widget)
+        self.prime_factors = QLabel(self.layoutWidget)
         self.prime_factors.setObjectName(u"prime_factors")
 
         self.horizontalLayout_2.addWidget(self.prime_factors)
@@ -67,9 +67,20 @@ class Ui_MainWindow(object):
         self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
-
-
         self.retranslateUi(MainWindow)
 
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.textBrowser.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">Please insert any number you would like to see the prime factorizaton of!</span></p></body></html>", None))
+        self.user_input.setText("")
+        self.go_button.setText(QCoreApplication.translate("MainWindow", u"Go!", None))
+        self.output_label.setText(QCoreApplication.translate("MainWindow", u"  Prime Factorization :", None))
+        self.prime_factors.setText("")
